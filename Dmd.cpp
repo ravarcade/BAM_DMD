1afd6db7e7cd106bc823460fb778f22cda7e28e9
Mikael Lagersson <mikael.lagersson@gmail.com>
2021-05-19 09:25:04 +0200

    Added a named pipe (futuredmd) and rapidjson message handling for the dmd and segment info. NOTE: rapidjson.1.0.2 needs to be included as a package, like how this is done for GLEW-static.2.1.0.3 - don't know if I need to push some more changes other than the vcxproj file for that?

--- Dmd.cpp
+++ Dmd.cpp
@@ -8,7 +8,229 @@
 #include "Dmd.h"
 #include "detours.h"
 
+#include "winnt.h"
+#include <process.h>		// for starting thread
+
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
 const char* cfgFilename = "BAM_DMD";
+std::string jsonMessage;
+int pipeTimeOut = 10;
+namespace {
+
+    void SendJsonMessage(void* dummy)
+    {
+
+        LPCWSTR lpszPipename = TEXT("\\\\.\\pipe\\futuredmd");
+        HANDLE hPipe;
+
+        hPipe = CreateFile(
+            lpszPipename,   // pipe name 
+            GENERIC_WRITE,  // write only access 
+            0,              // no sharing 
+            NULL,           // default security attributes
+            OPEN_EXISTING,  // opens existing pipe 
+            0,              // default attributes 
+            NULL);          // no template file 
+
+        WaitNamedPipe(lpszPipename, pipeTimeOut);
+
+        if (hPipe != INVALID_HANDLE_VALUE)
+        {
+            int datasize = jsonMessage.size();
+            DWORD dwSizeToWrite(datasize); DWORD dwWrittenSize(0);
+            while (dwSizeToWrite > 0)
+            {
+                WriteFile(hPipe, &jsonMessage[datasize - dwSizeToWrite], dwSizeToWrite, &dwWrittenSize, NULL);
+                dwSizeToWrite -= dwWrittenSize;
+                dwWrittenSize = 0;
+            }
+
+            CloseHandle(hPipe);
+        }
+
+    }
+
+    void RawImageToBuffer(CFpDmd dmd, byte* rgb_buf) {
+        // read raw image into rgb buffer
+        int size = dmd.width * dmd.height;
+        int idx = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            idx = i * 3;
+            int red = (dmd.rawDmdBuffer[(0 * size) + i] & 0xFF0000) >> 16; // Use as Alpha and Red
+
+            // using backColor as color if it's not a full color dmd
+            if (!dmd.isFullColor) {
+                rgb_buf[idx + 0] = (byte)(dmd.backColor[0] * red);
+                rgb_buf[idx + 1] = (byte)(dmd.backColor[1] * red);
+                rgb_buf[idx + 2] = (byte)(dmd.backColor[2] * red);
+            }
+            else {
+                rgb_buf[idx] = red; // Red
+                rgb_buf[idx + 1] = (dmd.rawDmdBuffer[(1 * size) + i] & 0xFF00) >> 8; // Green 
+                rgb_buf[idx + 2] = (dmd.rawDmdBuffer[(2 * size) + i] & 0xFF); // Blue
+            }
+        }
+    }
+
+    const char* statusAsString(int status) {
+        switch (status)
+        {
+        case 0: return "Running";
+        case 1: return "Started";
+        case 2: return "Stopped";
+        }
+        return "Unknown";
+    };
+
+    void PrepareMessage(std::vector<CFpDmd> dmds, int status) {
+        // document is the root of a json message
+        rapidjson::Document jsonContainer;
+
+        // define the document as an object rather than an array
+        jsonContainer.SetObject();
+
+        // must pass an allocator when the object may need to allocate memory
+        rapidjson::Document::AllocatorType& allocator = jsonContainer.GetAllocator();
+
+        jsonContainer.AddMember("Status", status, allocator);
+        rapidjson::Value statusDescription;
+        statusDescription.SetString(statusAsString(status), allocator);
+        jsonContainer.AddMember("StatusDescription", statusDescription, allocator);
+
+        if (status == 0) {
+			rapidjson::Value dispSegments = rapidjson::Value(rapidjson::kArrayType);
+			rapidjson::Value hudSegments = rapidjson::Value(rapidjson::kArrayType);
+
+            for (auto& dmd : dmds)
+            {
+                if (dmd.type == DISPDMD || dmd.type == HUDDMD) {
+                    // create an object to store all DMD info
+                    rapidjson::Value jdmd(rapidjson::kObjectType);
+                    jdmd.SetObject();
+
+                    jdmd.AddMember("name", dmd.name, allocator);
+                    jdmd.AddMember("type", dmd.type, allocator);
+                    jdmd.AddMember("isFullColor", dmd.isFullColor, allocator);
+                    jdmd.AddMember("isOnBackbox", dmd.isOnBackbox, allocator);
+                    jdmd.AddMember("width", dmd.width, allocator);
+                    jdmd.AddMember("height", dmd.height, allocator);
+                    jdmd.AddMember("color", *dmd.color, allocator);
+
+                    rapidjson::Value backColor(rapidjson::kObjectType); backColor.SetObject();
+                    backColor.AddMember("r", dmd.backColor[0], allocator);
+                    backColor.AddMember("g", dmd.backColor[1], allocator);
+                    backColor.AddMember("b", dmd.backColor[2], allocator);
+                    jdmd.AddMember("backColor", backColor, allocator);
+
+                    rapidjson::Value baseColor(rapidjson::kObjectType); baseColor.SetObject();
+                    baseColor.AddMember("r", dmd.baseColor[0], allocator);
+                    baseColor.AddMember("g", dmd.baseColor[1], allocator);
+                    baseColor.AddMember("b", dmd.baseColor[2], allocator);
+                    jdmd.AddMember("baseColor", baseColor, allocator);
+
+                    rapidjson::Value tmpColor(rapidjson::kObjectType); tmpColor.SetObject();
+                    tmpColor.AddMember("r", dmd.tmpColor[0], allocator);
+                    tmpColor.AddMember("g", dmd.tmpColor[1], allocator);
+                    tmpColor.AddMember("b", dmd.tmpColor[2], allocator);
+                    jdmd.AddMember("tmpColor", tmpColor, allocator);
+
+                    int len = (dmd.width * dmd.height * 3);
+                    byte* imgBuffer = new byte[len];
+                    RawImageToBuffer(dmd, imgBuffer);
+                    rapidjson::Value rgbBufferValue;
+                    rgbBufferValue.SetString(reinterpret_cast<char*>(imgBuffer), len, allocator);
+                    jdmd.AddMember("rgbBuffer", rgbBufferValue, allocator);
+                    delete[] imgBuffer;
+
+                    if (dmd.type == DISPDMD) {
+                        jsonContainer.AddMember("DISPDMD", jdmd, allocator);
+                    }
+                    else {
+                        jsonContainer.AddMember("HUDDMD", jdmd, allocator);
+                    }
+
+                }
+                else if (dmd.type == DISPSEG || dmd.type == HUDSEG) {
+                    // create an object to store an individual segment
+                    rapidjson::Value segment(rapidjson::kObjectType);
+                    segment.SetObject();
+
+                    segment.AddMember("name", dmd.name, allocator);
+                    segment.AddMember("type", dmd.type, allocator);
+                    segment.AddMember("isFullColor", dmd.isFullColor, allocator);
+                    segment.AddMember("isOnBackbox", dmd.isOnBackbox, allocator);
+
+					segment.AddMember("color", *dmd.color, allocator);
+
+					//rapidjson::Value backColor(rapidjson::kObjectType); backColor.SetObject();
+					//backColor.AddMember("r", dmd.backColor[0], allocator);
+					//backColor.AddMember("g", dmd.backColor[1], allocator);
+					//backColor.AddMember("b", dmd.backColor[2], allocator);
+					//segment.AddMember("BackColor", backColor, allocator);
+
+					//rapidjson::Value baseColor(rapidjson::kObjectType); baseColor.SetObject();
+					//baseColor.AddMember("r", dmd.baseColor[0], allocator);
+					//baseColor.AddMember("g", dmd.baseColor[1], allocator);
+					//baseColor.AddMember("b", dmd.baseColor[2], allocator);
+					//segment.AddMember("BaseColor", baseColor, allocator);
+
+					rapidjson::Value tmpColor(rapidjson::kObjectType); tmpColor.SetObject();
+					tmpColor.AddMember("r", dmd.tmpColor[0], allocator);
+					tmpColor.AddMember("g", dmd.tmpColor[1], allocator);
+					tmpColor.AddMember("b", dmd.tmpColor[2], allocator);
+					segment.AddMember("tmpColor", tmpColor, allocator);
+
+                    segment.AddMember("segAlign", dmd.segAlign, allocator);
+                    segment.AddMember("segCharShapes", *dmd.segCharShapes, allocator);
+                    segment.AddMember("segLength", dmd.segLength, allocator);
+                    segment.AddMember("segType", dmd.segType, allocator);
+					
+					//byte* imgBuffer = new byte[len];
+					//RawImageToBuffer(dmd, imgBuffer);
+					//segdmd.AddMember("segRawBuffer", dmd.segRawBuffer, allocator);
+                    //segdmd.AddMember("segRawBufferWithEffects", dmd.segRawBufferWithEffects, allocator);
+
+                    rapidjson::Value segText;
+                    segText.SetString(dmd.segText, allocator);
+                    segment.AddMember("segText", segText, allocator);
+
+					if (dmd.type == DISPSEG) {
+						dispSegments.PushBack(segment, allocator);
+					}
+					else {
+						hudSegments.PushBack(segment, allocator);
+					}
+
+
+                }
+            }
+
+            if (!dispSegments.Empty()) {
+                // add display segments to the JSON document
+                jsonContainer.AddMember("DISPSEG", dispSegments, allocator);
+            }
+			if (!hudSegments.Empty()) {
+				// add hud segments to the JSON document
+				jsonContainer.AddMember("HUDSEG", hudSegments, allocator);
+			}
+        }
+
+        rapidjson::StringBuffer buffer;
+        rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+        jsonContainer.Accept(writer);
+
+        jsonMessage = buffer.GetString();
+
+        rapidjson::Document().Swap(jsonContainer);  // <-- swap with temporary to wipe out memory pool
+    }
+
+}
+
 
 CDmd::CDmd(HMODULE hModule, HMODULE bam_module)
 	: hModule(hModule)
@@ -45,12 +267,23 @@ void CDmd::OnPluginStart()
 
 			}
 		});
+
+    //Send status Started
+    PrepareMessage(dmds, 1);
+    pipeTimeOut = 10;
+    _beginthread(SendJsonMessage, 0, NULL);
 }
 
 void CDmd::OnPluginStop()
 {
 	dmdCom.reset();  // destroy COM object
 	BAM::helpers::save(cfgFilename, &cfg, sizeof(cfg));
+
+    //Send status Stopped
+    PrepareMessage(dmds, 2);
+    pipeTimeOut = 500;
+    // Send 
+    _beginthread(SendJsonMessage, 0, NULL);
 }
 
 void CDmd::OnSwapBuffer(HDC hDC)
@@ -67,6 +300,10 @@ void CDmd::OnSwapBuffer(HDC hDC)
 		dmd.refresh();
 		dmd.dump();
 	}
+
+    PrepareMessage(dmds, 0);
+    pipeTimeOut = 10;
+    _beginthread(SendJsonMessage, 0, NULL);
 }
 
 IDispatch* CDmd::GetDmdCom()
